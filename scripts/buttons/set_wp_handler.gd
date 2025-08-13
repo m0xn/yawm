df21e55246c43bc0daa933f1954c829c8ab2b58c
m0xn <moon0xcoder@gmail.com>
2025-08-13 09:16:40 +0200

    Added support for more complex set and open wp cmds
    Now commands are split by spaces to separate the system
    cmd from its args.
    Use case: "<interpreter> <script.ext>" for sh or lua files
    (like for my sway env) as to make the user experience better
    since it won't have to grant execute permissions for the
    script to run.

--- scripts/buttons/set_wp_handler.gd
+++ scripts/buttons/set_wp_handler.gd
@@ -1,18 +1,23 @@
 extends Button
 
 func _ready() -> void:
-	# NOTE: Implement splitting by spaces to form more complex commands
 	button_down.connect(func ():
-		var set_wp_cmd = AppData.settings.get_value("cmds", "set_wp_cmd")
+		var raw_cmd = AppData.settings.get_value("cmds", "set_wp_cmd")
 		var wps_dir = AppData.settings.get_value("dirs", "wps_dir")
 
 		if AppData.currently_selected_thumbnail == null:
 			Utils.Debug.log_msg(Types.DT.ERROR, tr("DBG_MISSING_SELECTED_WP") % [tr("ADD_WP_BTN")])
 			return
+
+		var cmd_args = raw_cmd.split(" ")
+		var cmd = cmd_args[0]
+		cmd_args.remove_at(0)
+
+		cmd_args.append(wps_dir.path_join(AppData.currently_selected_thumbnail.filename))
 	
 		var output: Array
-		if OS.execute(set_wp_cmd, [wps_dir.path_join(AppData.currently_selected_thumbnail.filename)], output, true) != 0:
-			var full_cmd = "[b]" + set_wp_cmd + "[/b]" + " " + wps_dir.path_join(AppData.currently_selected_thumbnail.filename)
-			var error = "\n\n[bgcolor=black]%s[/bgcolor]" % [output.reduce(Utils.Str.flatten_output)]
+		if OS.execute(cmd, cmd_args, output, true) != 0:
+			var full_cmd = "[b]" + cmd + "[/b]" + cmd_args.reduce(Utils.Str.flatten_array)
+			var error = "\n\n[bgcolor=black]%s[/bgcolor]" % [output.reduce(Utils.Str.flatten_array)]
 			Utils.Debug.log_msg(Types.DT.ERROR, tr("DBG_INVALID_SET_WP_CMD") % [full_cmd, tr("STTS_CMDS_HEADER_LB"), tr("STTS_SET_AS_WP_SCRIPT_LB")] + error)
 	)
