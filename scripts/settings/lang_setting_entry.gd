c352f72507b838ec4ad21b24c0993cb5c3ecef2c
m0xn <moon0xcoder@gmail.com>
2025-08-18 10:14:55 +0200

    Changed the locale directly on the TranslationServer

--- scripts/settings/lang_setting_entry.gd
+++ scripts/settings/lang_setting_entry.gd
@@ -2,5 +2,8 @@ extends "base_setting_entry.gd"
 
 func _on_value_change(new_value: Variant) -> void:
 	super(new_value)
-	ProjectSettings.set("internationalization/locale/test", editable_node.get_item_text(new_value))
-	ProjectSettings.save()
+	TranslationServer.set_locale(editable_node.get_item_text(new_value))
+
+func _ready() -> void:
+	super()
+	TranslationServer.set_locale(editable_node.get_item_text(AppData.settings.get_value(stts_section, stts_name)))
