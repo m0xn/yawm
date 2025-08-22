b5c38b1ecaff54ec9ca8c0b4ae52afc729d49558
m0xn <moon0xcoder@gmail.com>
2025-08-22 19:51:45 +0200

    Minor fixes in the new version

--- scripts/wp_drag_handler.gd
+++ scripts/wp_drag_handler.gd
@@ -4,6 +4,17 @@ func _ready() -> void:
 	get_window().files_dropped.connect(load_dropped_files_into_viewport)
 
 func load_dropped_files_into_viewport(dropped_files: PackedStringArray) -> void:
+	var valid_files: PackedStringArray
+	for file in dropped_files:
+		if Utils.Format.img_format(file) == "":
+			continue
+
+		valid_files.append(file)
+
+	if len(valid_files) == 0:
+		Utils.Debug.log_msg(Types.DT.ERROR, tr("DBG_UNSUPPORTED_FILE_FORMAT"))
+		return
+
 	var import_window_ref = get_parent().get_node_or_null("WPImportWND")
 
 	if import_window_ref == null:
